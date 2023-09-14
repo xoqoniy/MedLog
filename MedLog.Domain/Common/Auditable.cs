9075216fd6dd829f3adf3cd032c0a52ae2cadc7e
Safarmurod7 <sm.ashurov7@gmail.com>
2023-09-14 07:39:25 +0500

    Auditable Id Auto generated

--- MedLog.Domain/Common/Auditable.cs
+++ MedLog.Domain/Common/Auditable.cs
@@ -1,10 +1,12 @@
 ﻿
 
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace MedLog.Domain.Common;
 
 public abstract class Auditable
 {
-    []
+    [BsonId]
     public int Id { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? LastUpdatedAt { get; set; }
