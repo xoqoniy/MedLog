4970d959fe778f6ae4bb9feeb2c5096d5f840ffb
Safarmurod7 <sm.ashurov7@gmail.com>
2023-12-10 18:52:43 +0500

    GridFS package downloaded

--- MedLog.Domain/Entities/PatientRecord.cs
+++ MedLog.Domain/Entities/PatientRecord.cs
@@ -10,9 +10,9 @@ public class PatientRecord : Auditable
     public IFormFile File { get; set; }
     public string Diagnosis { get; set; }
     public string Symptoms { get; set; }
-    public string Medications { get; set; }
-    public string Procedures {  get; set; }
-    public string Allergies { get; set; }
-    public string Description { get; set; }
+    public string? Medications { get; set; }
+    public string? Procedures {  get; set; }
+    public string? Allergies { get; set; }
+    public string? Description { get; set; }
 
 }
