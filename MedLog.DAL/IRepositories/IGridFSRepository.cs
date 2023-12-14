83693b7bd271bb719250e4cd9e663ba9560f698f
Safarmurod7 <sm.ashurov7@gmail.com>
2023-12-14 20:01:45 +0500

    GridFS repository

---
+++ MedLog.DAL/IRepositories/IGridFSRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using MedLog.Domain.Common;
+using MongoDB.Bson;
+
+namespace MedLog.DAL.IRepositories
+{
+    public interface IGridFSRepository<T> where T : Auditable, IEntityWithObjectId
+    {
+        Task<T> CreateAsync(T entity, Stream fileStream, string fileName);
+        Task<ObjectId> UploadFileAsync(Stream fileStream, string fileName);
+        Task<Stream> DownloadFileAsync(string fileId);
+    }
+}
--- MedLog.Domain/Entities/PatientRecord.cs
+++ MedLog.Domain/Entities/PatientRecord.cs
@@ -1,18 +1,19 @@
 ﻿using MedLog.Domain.Common;
 using Microsoft.AspNetCore.Http;
+using MongoDB.Bson;
 
 namespace MedLog.Domain.Entities;
 
-public class PatientRecord : Auditable
+public class PatientRecord : Auditable, IEntityWithObjectId
 {
     public Staff? Staff { get; set; }
     public User? User { get; set; }
-    public IFormFile File { get; set; }
+    public ObjectId FileId { get; set; } // ObjectId of the file in GridFS
     public string Diagnosis { get; set; }
     public string Symptoms { get; set; }
-    public string? Medications { get; set; }
-    public string? Procedures {  get; set; }
-    public string? Allergies { get; set; }
-    public string? Description { get; set; }
+    public string Medications { get; set; }
+    public string Procedures { get; set; }
+    public string Allergies { get; set; }
+    public string Description { get; set; }
 
 }
