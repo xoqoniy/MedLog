ea30e208bf3925cf8fd1cfa1182971c6e443e463
Ashurov Safarmurod <sm.ashurov7@gmail.com>
2024-05-24 21:20:00 +0500

    FileRepository and FileEntity have been modified

--- MedLog.DAL/IRepositories/IFileRepository.cs
+++ MedLog.DAL/IRepositories/IFileRepository.cs
@@ -1,12 +1,13 @@
-﻿using System.IO;
+﻿using MedLog.Domain.Entities;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace MedLog.DAL.IRepositories
 {
     public interface IFileRepository
     {
-        Task<string> UploadFileAsync(Stream fileStream, string fileName);
-        Task<Stream> DownloadFileAsync(string id);
+        Task<FileEntity> UploadFileAsync(FileEntity file);
+        Task<FileEntity> DownloadFileAsync(string id);
         Task DeleteFileAsync(string id);
     }
 }
--- MedLog.DAL/Repositories/FileRepository.cs
+++ MedLog.DAL/Repositories/FileRepository.cs
@@ -10,36 +10,71 @@ using MongoDB.Driver.Core.Configuration;
 using Microsoft.Extensions.Options;
 using MedLog.DAL.IRepositories;
 
-namespace MedLog.DAL.Repositories
+namespace MedLog.DAL.Repositories;
+
+public class FileRepository : IFileRepository
 {
-    public class FileRepository : IFileRepository
+    private readonly IMongoDatabase _database;
+    private readonly string _fileStorage;
+    private readonly GridFSBucket fsBucket;
+
+    public FileRepository(IOptions<MongoDbSettings> mongodbSettings, IMongoDatabase database)
     {
-        private readonly IMongoDatabase _database;
-        private readonly string _fileStorage;
-        private readonly GridFSBucket fsBucket;
-
-        public FileRepository(IOptions<MongoDbSettings> mongodbSettings, IMongoDatabase database)
-        {
-            this._database = database;
-            _fileStorage = mongodbSettings.Value.FilesStorage;
-            fsBucket = new GridFSBucket(database, new GridFSBucketOptions { BucketName = _fileStorage });
-        }
-        public async Task DeleteFileAsync(string id)
-        {
-            await fsBucket.DeleteAsync(ObjectId.Parse(id));
-        }
-
-        public async Task<Stream> DownloadFileAsync(string id)
-        {
-            return await fsBucket.OpenDownloadStreamAsync(ObjectId.Parse(id));
-        }
-
-        
-        public async Task<string> UploadFileAsync(Stream fileStream, string fileName)
-        {
-            var id = await fsBucket.UploadFromStreamAsync(fileName, fileStream);
-            return id.ToString();
-        }
-
+        this._database = database;
+        _fileStorage = mongodbSettings.Value.FilesStorage;
+        fsBucket = new GridFSBucket(database, new GridFSBucketOptions { BucketName = _fileStorage });
     }
+
+    public async Task DeleteFileAsync(string id)
+    {
+        var objectId = new ObjectId(id);
+        await fsBucket.DeleteAsync(objectId);
+    }
+
+    public async Task<FileEntity> DownloadFileAsync(string id)
+    {
+        ObjectId objectId = new ObjectId(id);
+        var fileInfo = await fsBucket.Find(new BsonDocument("_id", id)).FirstOrDefaultAsync();
+        if (fileInfo == null)
+        {
+            throw new Exception("File is null or empty");
+        }
+        var stream = new MemoryStream();
+        await fsBucket.DownloadToStreamAsync(objectId, stream);
+        stream.Seek(0, SeekOrigin.Begin);
+        return new FileEntity
+        {
+            _id = id,
+            FileName = fileInfo.Filename,
+            ContentType = fileInfo.Metadata.GetValue("contentType", "application/octet-stream").AsString,
+            CreatedAt = fileInfo.UploadDateTime,
+            Content = stream
+        };
+    }
+
+    public async Task<FileEntity> UploadFileAsync(FileEntity file)
+    {
+        var options = new GridFSUploadOptions
+        {
+            Metadata = new BsonDocument
+            {
+
+                    { "filename" , file.FileName },
+                    { "contentType", file.ContentType },
+                    { "description", file.Description}
+            }
+        };
+
+        var fileId = await fsBucket.UploadFromStreamAsync(file.FileName, file.Content, options);
+        return new FileEntity
+        {
+            _id = fileId.ToString(),
+            FileName = file.FileName,
+            ContentType = file.ContentType,
+            Description = file.Description,
+            CreatedAt = DateTime.UtcNow
+        };
+        
+    }
+
 }
