63b036d25b7df518d606a62ff08aca6e0bfae49a
Ashurov Safarmurod <sm.ashurov7@gmail.com>
2024-06-01 17:27:05 +0500

    Route Configuration
    Converting api url to dash case

---
+++ MedLog/Extensions/RouteConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+#pragma warning disable
+namespace MedLog.Extensions
+{
+    public class RouteConfiguration : IOutboundParameterTransformer
+    {
+        public string TransformOutbound(object value)
+        {
+            // Slugify value
+            return value == null ? null : Regex.Replace(value.ToString(), "([a-z])([A-Z])", "$1-$2").ToLower();
+        }
+    }
+
+}
